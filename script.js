301d58cf781f5152341e0b2a3517b4682a880c2c
HStep20 <Stephens.j.hunter@protonmail.com>
2020-09-20 14:35:38 -0600

    ADD: HTML, CSS, JS structure for firefox addon. TODO: Wont run JS from extension popup, but will in live server

---
+++ script.js
@@ -0,0 +1,38 @@
+function generate() {
+    charset = ""
+    document.getElementById("outputbox").innerHTML = charset
+    if(document.getElementById("capital").checked){
+        charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
+    if(document.getElementById("lowercase").checked){
+        charset += "abcdefghijklmnopqrstuvwxyz"
+    }
+    if(document.getElementById("numbers").checked){
+        charset += "0123456789"
+    }
+    if(document.getElementById("symbols").checked){
+        charset += "!$&*-=?@~";
+    }
+    if(charset == ""){
+        charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!$&*-=?@~"
+    }
+    document.getElementById("outputbox").innerHTML = generatePassword(charset)
+    document.getElementById("clipboard").style.visibility = 'visible';
+}
+
+function generatePassword(charset){
+    length  = Math.floor((Math.random() * 10) + 15)
+    pwd = ""
+    for (i=0; i <= length; i++){
+        randomcspos = Math.floor((Math.random() * charset.length))
+        pwd += charset.charAt(randomcspos)
+    }
+    return pwd
+}
+
+function copyToClipboard(){
+    const texttocopy = document.getElementById("outputbox");
+    texttocopy.select();
+    document.execCommand('copy'); 
+    texttocopy.blur();
+}
